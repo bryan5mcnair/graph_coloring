28421374dd21cc60aeb5cc93a3da4ed0cff59325
Bryan McNair <Bryan5McNair@gmail.com>
2024-04-19 19:13:07 -0700

    Color the vertices of the graph

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
 def get_vertices(adj_list):
     return list(adj_list.keys())
@@ -8,19 +9,55 @@ def get_vertices(adj_list):
 def pretty_print_edges(edge_tuples):
     return [f"{v1}<-->{v2}" for v1, v2 in edge_tuples]
 
-def draw_graph(graph):
-    nx.draw(graph, with_labels=True, font_weight='bold')
+def draw_graph(graph, colors):
+    nx.draw(graph, with_labels=True, font_weight='bold', node_color=colors)
     plt.show()
+
+def get_colors(n):
+    colors = ["#808080"]
+    for i in range(n):
+        j = 0
+        while True:
+            r, g, b = tuple(list(np.random.choice(range(256), size=3)))
+            color = "#{:02x}{:02x}{:02x}".format(r,g,b)
+            if (color not in colors or j == 20) and color != "#808080":
+                colors.append(color)
+                break
+            j += 1
+    return colors
+
+def is_valid_color_num(num, maxNum):
+    return num.isdigit() and int(num) > 0 and int(num) <= maxNum
+
+def update_graph_colors(graph, colors, coloring):
+    color_map = []
+    for node in graph:
+        color_map.append(colors[coloring[node]])
+    return color_map
 
 def construct_graph():
     graph = nx.Graph()
     vertices = dict()
+    coloring = dict()
+
     e_list = []
 
     optionExists = True
 
+    colors = []
+    while True:
+        color_num = input("How many colors do you want to use? ")
+        if is_valid_color_num(color_num, float("inf")):
+            color_num = int(color_num)
+            colors = get_colors(color_num)
+            break
+        print("Try again. Not a valid input.")
+    
     while True:
         os.system('cls')
+
+        color_map = update_graph_colors(graph, colors, coloring)
+
         if not optionExists:
             print(f"The option you chose does not exist. Choose a number between 1 and 5.\n")
         else:
@@ -50,8 +87,10 @@ def construct_graph():
             while True:
                 v = input("Enter the vertex name: ")
                 v = v.lower()
-                graph.add_node(v)
+                
                 if v not in vertices:
+                    graph.add_node(v)
+                    coloring[v] = 0 # set default node color to gray
                     vertices[v] = []
                     break
                 else:
@@ -79,7 +118,25 @@ def construct_graph():
                 if askEdgeAgain.lower() not in ['y', 'yes']:
                     break
         elif option == 3:
-            draw_graph(graph)
+            while True:
+                v = input("\nEnter the vertex that you want to color: ")
+                v = v.lower()
+
+                if v not in vertices:
+                    print(f"\nTry again. Vertex {v} does not exist.")
+                else:
+                    color = input(f"From the {color_num} colors, choose a color between 1 and {color_num}, inclusive, to color vertex {v}: ")
+                    if not is_valid_color_num(color, color_num):
+                        print(f"\nTry again. This is not a valid color. Choose a color between 1 and {color_num}.")
+                    else:
+                        color = int(color)
+                        coloring[v] = color
+                        break
+                askColorAgain = input("Do you want to continue with working with colors (Y/N)? ")
+                if askColorAgain.lower() not in ['y', 'yes']:
+                    break
+        elif option == 4:
+            draw_graph(graph, color_map)
         elif option == 5:
             print("The End :)")
             break
