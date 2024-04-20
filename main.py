547f8e602152151facbab49f8a9b76ad374ebd87
Bryan McNair <Bryan5McNair@gmail.com>
2024-04-20 05:03:52 -0700

    Add term for chromatic symmetric function of graph

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def get_vertices(adj_list):
 def pretty_print_edges(edge_tuples):
     return [f"{v1}<-->{v2}" for v1, v2 in edge_tuples]
 
-def draw_graph(graph, colors, chromatic_polynomial, coloring_possible, color_num, is_valid_coloring):
+def draw_graph(graph, colors, chromatic_polynomial, coloring_possible, color_num, is_valid_coloring, term_statement):
     num_ways = eval(str(chromatic_polynomial).replace("x", str(color_num)))
     is_coloring_possible = f"Not able to color this graph with {color_num} colors" if not coloring_possible else f"There are {num_ways} ways to color this graph with {color_num} colors."
     nx.draw(graph, with_labels=True, font_weight='bold', node_color=colors)
     plt.text(0, 0, f"Chromatic Polynomial: {chromatic_polynomial}", horizontalalignment='center', verticalalignment='center')
     plt.text(0, -0.1, is_coloring_possible, horizontalalignment='center', verticalalignment='center')
     plt.text(0, -0.2, is_valid_coloring, horizontalalignment='center', verticalalignment='center')
+    plt.text(0, -0.3, term_statement, horizontalalignment='center', verticalalignment='center')
     plt.show()
 
 def get_colors(n):
@@ -37,10 +38,15 @@ def is_valid_color_num(num, maxNum):
     return num.isdigit() and int(num) > 0 and int(num) <= maxNum
 
 def update_graph_colors(graph, colors, coloring):
+    color_count = dict()
     color_map = []
     for node in graph:
+        if coloring[node] not in color_count:
+            color_count[coloring[node]] = 1
+        else:
+            color_count[coloring[node]] += 1
         color_map.append(colors[coloring[node]])
-    return color_map
+    return (color_map, color_count)
 
 def is_coloring_possible(chrom_poly, x):
     return eval(str(chrom_poly).replace("x", str(x))) > 0
@@ -53,7 +59,6 @@ def valid_coloring(adjList, coloring):
             if coloring[v1] == coloring[v2[1]]:
                 return "This is not a valid coloring of the graph"
     return "This is a valid coloring of the graph"
-
 
 def construct_graph():
     plt.rcParams.update({'font.size': 14})
@@ -78,7 +83,7 @@ def construct_graph():
     while True:
         os.system('cls')
 
-        color_map = update_graph_colors(graph, colors, coloring)
+        color_map, color_count = update_graph_colors(graph, colors, coloring)
 
         if not optionExists:
             print(f"The option you chose does not exist. Choose a number between 1 and 5.\n")
@@ -108,9 +113,11 @@ def construct_graph():
         if option == 1:
             while True:
                 v = input("Enter the vertex name: ")
-                v = v.lower()
+                v = v.lower().strip()
                 
-                if v not in vertices:
+                if v == "":
+                    print("\nTry again. Blank vertex is not allowed.")
+                elif v not in vertices:
                     graph.add_node(v)
                     coloring[v] = 0 # set default node color to gray
                     vertices[v] = []
@@ -120,9 +127,9 @@ def construct_graph():
         elif option == 2:
             while True:
                 v1 = input("\nEnter the start vertex for the edge: ")
-                v1 = v1.lower()
+                v1 = v1.lower().strip()
                 v2 = input("Enter the ending vertex for the edge: ")
-                v2 = v2.lower()
+                v2 = v2.lower().strip()
 
                 if v1 == v2:
                     print("\nTry again. The start vertex can't equal the end vertex.")
@@ -137,17 +144,19 @@ def construct_graph():
                     e_list.append((v1,v2))
                     break
                 askEdgeAgain = input("Do you want to continue with working with edges (Y/N)? ")
+                askEdgeAgain = askEdgeAgain.strip()
                 if askEdgeAgain.lower() not in ['y', 'yes']:
                     break
         elif option == 3:
             while True:
                 v = input("\nEnter the vertex that you want to color: ")
-                v = v.lower()
+                v = v.lower().strip()
 
                 if v not in vertices:
                     print(f"\nTry again. Vertex {v} does not exist.")
                 else:
                     color = input(f"From the {color_num} colors, choose a color between 1 and {color_num}, inclusive, to color vertex {v}: ")
+                    color = color.strip()
                     if not is_valid_color_num(color, color_num):
                         print(f"\nTry again. This is not a valid color. Choose a color between 1 and {color_num}.")
                     else:
@@ -155,13 +164,30 @@ def construct_graph():
                         coloring[v] = color
                         break
                 askColorAgain = input("Do you want to continue with working with colors (Y/N)? ")
+                askColorAgain = askColorAgain.strip()
                 if askColorAgain.lower() not in ['y', 'yes']:
                     break
         elif option == 4:
             chrom_poly = nx.chromatic_polynomial(graph)
             coloring_possible = is_coloring_possible(chrom_poly, color_num)
             is_valid_coloring = valid_coloring(vertices, coloring)
-            draw_graph(graph, color_map, chrom_poly, coloring_possible, color_num, is_valid_coloring)
+            coefficient = 1
+            terms = ""
+            colored = True
+            for color in color_count:
+                if color == 0:
+                    terms = "The term for the chromatic symmetric function is unable to be calculated so far."
+                    coefficient = ""
+                    colored = False
+                    break
+                else:
+                    coefficient *= int(color_count[color])
+                    terms += f"x_{color}"
+            coefficient = "" if coefficient == 1 else str(coefficient)
+            term = coefficient + terms
+            if colored:
+                term = f"The term of the chromatic symmetric function for this graph would be {term}."
+            draw_graph(graph, color_map, chrom_poly, coloring_possible, color_num, is_valid_coloring, term)
         elif option == 5:
             print("The End :)")
             break
